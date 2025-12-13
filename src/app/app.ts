46950195242f1ad9cb10f2460fc236727880f102
Kristijan <kjagunec@gmail.com>
2025-12-13 16:50:18 +0100

    Seperated the page into nav and main component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Nav} from './nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('dance-web-app');
-}
+export class App {}
