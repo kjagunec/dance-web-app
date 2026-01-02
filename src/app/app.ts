6fc54120f1c22a664e03ccf0422f0a47594bb6ce
Kristijan <kjagunec@gmail.com>
2026-01-02 19:47:09 +0100

    Separated the footer into a component and added google maps view

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Nav} from './nav/nav';
+import {Footer} from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav],
+  imports: [RouterOutlet, Nav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
